61bf715038896c80f1630f3b1f85160e97259395
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2025-08-07 13:17:16 +0300

    add new toolchain

--- wasm-project/check.sh
+++ wasm-project/check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 RUSTFLAGS="--emit=llvm-ir" \
-rustup run nightly-2025-04-22 cargo rustc \
+rustup run nightly-2025-08-07 cargo rustc \
 --target=wasm32v1-none \
 --color=always \
 --manifest-path="$(pwd)/Cargo.toml" \
@@ -13,7 +13,7 @@ rustup run nightly-2025-04-22 cargo rustc \
 -C save-temps \
 --print link-args
 
-rustup run nightly-2025-04-22 cargo run \
+rustup run nightly-2025-08-07 cargo run \
 --release \
 --manifest-path=../wasm-checker/Cargo.toml \
 -- \
