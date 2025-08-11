2e3906d6e8418a330c981cb05f044ec84069ee51
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2025-08-11 10:20:45 +0300

    add more verbose

--- wasm-project/check.sh
+++ wasm-project/check.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 RUSTFLAGS="--emit=llvm-ir" \
-rustup run nightly-2025-08-09 cargo rustc \
+RUSTC_LOG="rustc_codegen_ssa::back::link=info" \
+rustup run nightly-2025-08-11 cargo rustc \
 --target=wasm32v1-none \
 --color=always \
 --manifest-path="$(pwd)/Cargo.toml" \
@@ -9,11 +10,12 @@ rustup run nightly-2025-08-09 cargo rustc \
 --verbose \
 -- \
 -C link-arg=--import-memory \
+-C link-arg=--verbose \
 -C linker-plugin-lto \
 -C save-temps \
 --print link-args
 
-rustup run nightly-2025-08-09 cargo run \
+rustup run nightly-2025-08-11 cargo run \
 --release \
 --manifest-path=../wasm-checker/Cargo.toml \
 -- \
